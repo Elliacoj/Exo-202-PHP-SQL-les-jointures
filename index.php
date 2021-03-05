de72b6f00f40155d1371f83c29e2b124ba2dda67
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-03-05 13:28:30 +0100

    modif

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once "./Classes/DB.php";
 
 /**
  * 1. Commencez par importer le script SQL disponible dans le dossier SQL.
@@ -17,14 +18,46 @@
  */
 
 // TODO Votre code ici.
+$db = DB::getInstance();
 
+$search = $db->prepare("
+                                SELECT article.id, article.title, article.content, categorie.name
+                                FROM article
+                                INNER JOIN categorie on article.category_fk = categorie.id
+");
+
+$state = $search->execute();
+
+if($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
+}
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
  */
 
 // TODO Votre code ici.
+$db = DB::getInstance();
 
+$search = $db->prepare("
+                                SELECT ar.id as i, ar.title as t, ar.content as c, ca.name as category
+                                FROM article as ar
+                                INNER JOIN categorie as ca on ar.category_fk = ca.id
+");
+
+$state = $search->execute();
+
+if($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
+}
 
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
@@ -33,3 +66,20 @@
  */
 
 // TODO Votre code ici.
+
+$search = $db->prepare("
+                                SELECT co.content, ut.firstname, ut.lastname
+                                FROM commentaire as co
+                                LEFT JOIN utilisateur as ut on co.user_fk = ut.id
+");
+
+$state = $search->execute();
+
+if($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
+}
+
